1b628cf564a09e6b90f4d0b43629ee536e839c73
Justin Bui <legioncactus@berkeley.edu>
2017-10-01 10:31:44 -0700

    Adding Reaction Time
    Adding David's code for reaction time

---
+++ reaction/app/src/main/java/gmu/braintech/react/AboutActivity.java
@@ -0,0 +1,44 @@
+package gmu.braintech.react;
+
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+/**
+ *
+ * @author wang.david.501@gmail.com
+ */
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mailto("braintech@gmu.edu");
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getWindow().setStatusBarColor(Color.parseColor("#BA1AEE"));
+    }
+
+
+    private void mailto(String email) {
+        Intent launchBrowser = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto",email, null));
+        startActivity(launchBrowser);
+    }
+}
