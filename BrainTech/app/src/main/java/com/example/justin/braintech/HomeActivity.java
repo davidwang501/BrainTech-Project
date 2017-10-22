2d4c386756200a3e998eafa4ea609f729c0e119c
Justin Bui <legioncactus@berkeley.edu>
2017-10-22 16:18:34 -0700

    Successfully
    Finally succeeded in integrating

--- BrainTech/app/src/main/java/com/example/justin/braintech/HomeActivity.java
+++ BrainTech/app/src/main/java/com/example/justin/braintech/HomeActivity.java
@@ -7,6 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.support.annotation.RequiresApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
